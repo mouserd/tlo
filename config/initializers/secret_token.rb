5437bfbb71b3409eae3dd235befd78f0ba0be4c2
David Mouser <david.mouser@gmail.com>
2014-03-24 20:47:18 +1100

    Initial revision.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tlo::Application.config.secret_token = '26ca61b8f291bd22d29f609bd4a50b97baeffc0ebbd7778cdf433d0cbfe7e97a1f6d15f1ef761c33624426b8245607cfe995009ba7aa55851c21953e911b501f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tlo::Application.config.session_store :cookie_store, key: '_tlo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tlo::Application.config.session_store :active_record_store
